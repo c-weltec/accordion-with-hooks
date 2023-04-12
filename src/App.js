60622f633f8d245b0df24df800bfea2670f0df69
c-weltec <chintan.weltec@gmail.com>
2023-04-12 18:54:00 +0530

    inital commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Accordion from "./components/LiftingStateUp/Accordion";
+import PanelA from "./components/LiftingStateUp/PanelA";
+import PanelB from "./components/LiftingStateUp/PanelB";
+
+function App() {
+  return (
+    <>
+      <Accordion />
+    </>
+  );
+}
+export default App;
